7a80e970ca9858687106995e26a0fa5ac51d9c2c
AutoiPacket <james.braswell@autoipacket.com>
2020-04-21 18:01:02 -0400

    final commit for part 1 exercises.

---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+function randNum(max) {
+  return (Math.floor(Math.random() * Math.floor(max)));
+}
+
+const Button = (props) => {
+  return(
+  <button onClick={props.handleClick}>{props.text}</button>
+  )
+}
+
+const Votes = (props) => {
+  return(
+  <p>This Anecdote has {props.votes} votes.</p>
+  )
+}
+
+const App = (props) => {
+  const [votes, setVotes] = useState(
+    {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+    )
+  const [selected, setSelected] = useState(0)
+  const setRandom = () => {
+    setSelected(randNum(anecdotes.length))
+  }
+  
+  const clickVote = () => {
+    const newVotes = {...votes}
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
+
+  const voteArray = Object.values(votes);
+  const maxVote = Math.max(...voteArray) // You need to find the key of the max value
+  const maxIndex = voteArray.findIndex(vote => vote === maxVote)
+  console.log(votes)
+  console.log(maxIndex)
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <Votes votes={votes[selected]}/>
+      <div>
+      <Button handleClick={clickVote} text='Vote'/>
+      <Button handleClick={setRandom} text='Next Anecdote'/></div>
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[maxIndex]}
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
